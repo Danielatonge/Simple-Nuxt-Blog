b3e4134acc6c2d6a9265baef06b6b944243bfc76
Daniel Atonge <elamboatonge@gmail.com>
2021-12-22 22:15:26 +0300

    finishes basics

---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="admin-new-post-page">
+    <section class="new-post-form">
+      <AdminPostForm @submit="onSubmitted" />
+    </section>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  layout: 'admin',
+  methods: {
+    async onSubmitted(formData) {
+      const response = await axios.post(
+        'https://nuxt-blog-9fbb2-default-rtdb.firebaseio.com/posts.json',
+        formData
+      )
+      console.log(response)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.new-post-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .new-post-form {
+    width: 500px;
+  }
+}
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
-  <Tutorial />
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+    <PostList />
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage',
-}
+export default {}
 </script>
